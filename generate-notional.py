a7ac12d2d02e647ef8df6d630cf10e23bafada94
acox37 <acox37@gatech.edu>
2023-11-28 17:56:20 -0500

    feat(generate_notional): add technology category

--- generate-notional.py
+++ generate-notional.py
@@ -1,14 +1,18 @@
-from random import shuffle
+from random import shuffle, randint
 from FAMS.model_rankings import Technology, Order, Ranking
 from time import time
 
 num_metrics = 5
 num_techs = 15
+num_categories = num_metrics // min((num_metrics // 2, 5))
 num_samples = num_techs
 
 """ Generate and process rankings """
 t0 = time()
-technologies = [Technology(id_=i) for i in range(num_techs)]
+categories = [f'Category {i}' for i in range(num_categories)]
+technologies = [Technology(id_=i,
+                           category=categories[randint(0, num_categories - 1)])
+                for i in range(num_techs)]
 for metric_id in range(num_metrics):
     metric = f'Metric {metric_id}'
     samples = list()
